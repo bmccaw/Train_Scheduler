7539a1370d8dd431201b61423474acdd9d82e505
Green Man <brendan.mccaughey@gmail.com>
2018-10-29 12:39:37 -0400

    User input is sending to the database. This task is complete.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,6 @@
+$(document).ready(function() {
+
+
 //Connect to Firebase
 var config = {
     apiKey: "AIzaSyAFc2wIna9YCdRZbW8Hu3rUdnSUMbI89Pk",
@@ -8,3 +11,32 @@ var config = {
     messagingSenderId: "798291269778"
   };
   firebase.initializeApp(config);
+
+//establish global variables
+let database = firebase.database();
+let addTrain;
+let addDest;
+let addTtime;
+let addFreq;
+
+//On click event to grab user input and send to the database
+$("#submit").on("click", function(event){
+        event.preventDefault();
+    
+          addTrain = $("#trainname").val().trim();
+          addDest = $("#destination").val().trim();
+          addTtime = $("#traintime").val().trim();
+          addFreq = $("#frequency").val().trim();
+    
+          database.ref().push({
+              train: addTrain,
+              destination: addDest,
+              train_time: addTtime,
+              frequency: addFreq,
+              dataAdded: firebase.database.ServerValue.TIMESTAMP
+    
+          });
+    
+    })
+
+});
