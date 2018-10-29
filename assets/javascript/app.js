c2159c2d19d19bc95d5acf8b320dbd2b8c0bf9a9
Green Man <brendan.mccaughey@gmail.com>
2018-10-29 11:07:38 -0400

    HTML layout work.

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+//Connect to Firebase
+var config = {
+    apiKey: "AIzaSyAFc2wIna9YCdRZbW8Hu3rUdnSUMbI89Pk",
+    authDomain: "train-scheduler-6cf39.firebaseapp.com",
+    databaseURL: "https://train-scheduler-6cf39.firebaseio.com",
+    projectId: "train-scheduler-6cf39",
+    storageBucket: "train-scheduler-6cf39.appspot.com",
+    messagingSenderId: "798291269778"
+  };
+  firebase.initializeApp(config);
